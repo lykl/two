794a119d8970b09917f17a5f0ae288a1855a9a1c
abzhb <1076900186@qq.com>
2018-10-10 14:32:31 +0800

    zhb 10-10提交

--- php/htjh.php
+++ php/htjh.php
@@ -1,12 +1,12 @@
 <?php
     error_reporting(E_ALL ^ E_DEPRECATED);
-    $db=mysql_connect("localhost","root","123456");//连接数据库   (地址 用户名 密码)
+    $db=mysql_connect("localhost","root","");//连接数据库   (地址 用户名 密码)
     mysql_select_db("data",$db);//选择数据库 数据库名称 连接
     mysql_query("set names utf8");//设置编码格式
 
 
     $uname = $_REQUEST["myname"];
-    $upassward = $_REQUEST["mypassward"];
+    $upassword = $_REQUEST["mypassword"];
     $usex = $_REQUEST["mysex"];
     $uphone=$_REQUEST["myphone"];
 
@@ -22,7 +22,7 @@
             
 
         }else{
-            $sql3="INSERT INTO `my_data`(`my_id`, `my_name`, `my_passward`, `my_sex`, `my_phone`) VALUES (NULL, '$uname','$upassward','$usex','$uphone')";
+            $sql3="INSERT INTO `my_data`(`my_id`, `my_name`, `my_password`, `my_sex`, `my_phone`) VALUES (NULL, '$uname','$upassword','$usex','$uphone')";
             $count = mysql_query($sql3); // 增加 删除 修改 返回受影响的行数
             if($count==1){
                 echo '{"status":200,"msg":"注册成功","res":true}';
